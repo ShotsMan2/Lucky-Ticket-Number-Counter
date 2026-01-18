78e6b710fe6a722f0719851adc77a72424a102ac
Selim Efe <uysalselimefe@gmail.com>
2026-01-18 23:08:17 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,29 +3,30 @@
 
 int main()
 {
-        int sayi,i,kalan,b1,b2,b3,b4,sum;
+    int sayi, i, kalan, b1, b2, b3, b4, sum;
 
-    sum=0;
+    sum = 0;
 
-    for(i=1000;i<=9999;i++){
+    for (i = 1000; i <= 9999; i++)
+    {
 
-        b1=i/1000;
-        kalan=i%1000;
+        b1 = i / 1000;
+        kalan = i % 1000;
 
-        b2=kalan/100;
-        kalan=kalan%100;
+        b2 = kalan / 100;
+        kalan = kalan % 100;
 
-        b3=kalan/10;
-        kalan=kalan%10;
+        b3 = kalan / 10;
+        kalan = kalan % 10;
 
-        b4=kalan;
+        b4 = kalan;
 
-        if(b1+b2==b3+b4){
+        if (b1 + b2 == b3 + b4)
+        {
             sum++;
         }
     }
-    printf("sonuc: %d",sum);
-
+    printf("sonuc: %d", sum);
 
     return 0;
 }
